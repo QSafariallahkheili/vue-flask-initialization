f718b4f3b6221afb6a92b0dd12b38f2e34bfde16
Tobias Kusian <tobias.kusian@urban-codesign.com>
2022-06-23 16:11:27 +0200

    renamed buttons#

--- frontend/src/components/OSMButton.vue
+++ frontend/src/components/OSMButton.vue
@@ -1,10 +1,10 @@
 <template>
     <div style="z-index:999; position: absolute" class=" btn-vertical ">
         <button class=" btn btn-secondary mt-2 mx-2" @click="storeOSMData">
-            Store button
+            Store it!
         </button>
         <button class=" btn btn-primary mt-2 mx-2" @click="retrieveOSMData">
-            Retrieve button
+            Retrieve it!
         </button>
     </div>
 </template>
